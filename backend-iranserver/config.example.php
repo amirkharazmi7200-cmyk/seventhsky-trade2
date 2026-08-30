9bebd4fac0950cafe1fa3309375d4925526ebab1
amirkharazmi7200-cmyk <amirkharazmi7200@gmail.com>
2026-08-30 16:57:57 +0330

    Add IranServer backend config template

---
+++ backend-iranserver/config.example.php
@@ -0,0 +1,11 @@
+<?php
+return [
+  'db_host' => 'localhost',
+  'db_name' => 'CHANGE_ME',
+  'db_user' => 'CHANGE_ME',
+  'db_pass' => 'CHANGE_ME',
+  'api_token' => 'CHANGE_ME_TO_A_LONG_RANDOM_SECRET',
+  'allowed_origins' => [
+    'https://radar.7skytrade.com'
+  ],
+];
